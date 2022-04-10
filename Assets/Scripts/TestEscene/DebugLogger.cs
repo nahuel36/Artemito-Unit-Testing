6ecca5b1437d51a8b5ffdebc0db22e33bd97be45
nahuel36 <nahuel36gg@gmail.com>
2022-04-09 23:16:10 -0300

    refactor

--- Assets/Scripts/TestEscene/DebugLogger.cs
+++ Assets/Scripts/TestEscene/DebugLogger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Threading.Tasks;
 
-public class TestScene : MonoBehaviour, IInteraction
+public class DebugLogger : MonoBehaviour, IInteraction
 {
     public string log;
 
