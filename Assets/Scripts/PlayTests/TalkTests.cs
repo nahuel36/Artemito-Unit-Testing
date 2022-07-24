daa89270ddc05b54b4819254a977b663e9abdec7
nahuel36 <nahuel36gg@gmail.com>
2022-07-24 05:01:47 -0300

    talks tests, seteado SetUp and TearDown

--- Assets/Scripts/PlayTests/TalkTests.cs
+++ Assets/Scripts/PlayTests/TalkTests.cs
@@ -8,6 +8,9 @@ namespace Tests
 {
     public class TalkTests
     {
+        IMessageTalker talker;
+        ITextTimeCalculator textTimeCalculator;
+
         [TearDown]
         public void TearDown()
         {
@@ -18,16 +21,19 @@ namespace Tests
         public void SetUp()
         {
             InteractionManager.Instance.ClearAll();
+            GameObject go = new GameObject();
+            go.AddComponent<TMPro.TextMeshProUGUI>();
+            InteractionManager.Instance.ClearAll();
+            textTimeCalculator = new TextTimeCalculator();
+            talker = new LucasArtText(go.transform, textTimeCalculator);
         }
+
         // A Test behaves as an ordinary method
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
         public IEnumerator Talk()
         {
-            GameObject go = new GameObject();
-            go.AddComponent<TMPro.TextMeshProUGUI>();
-            IMessageTalker talker = new LucasArtText(go.transform, new TextTimeCalculator());
             talker.Talk("hello world", false);
             yield return null;
             Assert.AreEqual("hello world", talker.Text);
@@ -36,9 +42,6 @@ namespace Tests
         [UnityTest]
         public IEnumerator TalkAndSkip()
         {
-            GameObject go = new GameObject();
-            go.AddComponent<TMPro.TextMeshProUGUI>();
-            IMessageTalker talker = new LucasArtText(go.transform, new TextTimeCalculator());
             talker.Talk("hello world", true);
             talker.Skip();
             yield return new WaitForEndOfFrame();
@@ -48,37 +51,25 @@ namespace Tests
         [UnityTest]
         public IEnumerator TalkAndWaitToFinish()
         {
-            GameObject go = new GameObject();
-            go.AddComponent<TMPro.TextMeshProUGUI>();
-            ITextTimeCalculator calculator = new TextTimeCalculator();
-            IMessageTalker talker = new LucasArtText(go.transform, calculator);
             talker.Talk("hello world", true);
-            yield return new WaitForSeconds(calculator.CalculateTime("hello world"));
+            yield return new WaitForSeconds(textTimeCalculator.CalculateTime("hello world"));
             Assert.AreEqual("", talker.Text);
         }
 
         [UnityTest]
         public IEnumerator TalkAndWaitToFinishTwoMessages()
         {
-            GameObject go = new GameObject();
-            go.AddComponent<TMPro.TextMeshProUGUI>();
-            ITextTimeCalculator calculator = new TextTimeCalculator();
-            IMessageTalker talker = new LucasArtText(go.transform, calculator);
             InteractionTalk inter1 = new InteractionTalk();
             inter1.Queue(talker, "hello world", false, false);
             InteractionTalk inter2 = new InteractionTalk();
             inter2.Queue(talker, "message2", false, false);
-            yield return new WaitForSeconds(calculator.CalculateTime("hello world") + calculator.CalculateTime("message2"));
+            yield return new WaitForSeconds(textTimeCalculator.CalculateTime("hello world") + textTimeCalculator.CalculateTime("message2"));
             Assert.AreEqual("", talker.Text);
         }
 
         [UnityTest]
         public IEnumerator TalkAndSkipToNextMessage()
         {
-            GameObject go = new GameObject();
-            go.AddComponent<TMPro.TextMeshProUGUI>();
-            ITextTimeCalculator calculator = new TextTimeCalculator();
-            IMessageTalker talker = new LucasArtText(go.transform, calculator);
             InteractionTalk inter = new InteractionTalk();
             inter.Queue(talker, "hello world", true, false);
             InteractionTalk inter2 = new InteractionTalk();
@@ -92,25 +83,17 @@ namespace Tests
         [UnityTest]
         public IEnumerator TalkAndWaitToNextMessage()
         {
-            GameObject go = new GameObject();
-            go.AddComponent<TMPro.TextMeshProUGUI>();
-            ITextTimeCalculator calculator = new TextTimeCalculator();
-            IMessageTalker talker = new LucasArtText(go.transform, calculator);
             InteractionTalk inter = new InteractionTalk();
             inter.Queue(talker, "hello world", true, false);
             InteractionTalk inter2 = new InteractionTalk();
             inter2.Queue(talker, "message2", true, false);
-            yield return new WaitForSeconds(calculator.CalculateTime("hello world")); 
+            yield return new WaitForSeconds(textTimeCalculator.CalculateTime("hello world")); 
             Assert.AreEqual("message2", talker.Text);
         }
 
         [UnityTest]
         public IEnumerator TalkAndSkipTwoMessages()
         {
-            GameObject go = new GameObject("two");
-            go.AddComponent<TMPro.TextMeshProUGUI>();
-            ITextTimeCalculator calculator = new TextTimeCalculator();
-            IMessageTalker talker = new LucasArtText(go.transform, calculator);
             InteractionTalk inter = new InteractionTalk();
             inter.Queue(talker, "hello world", true, false);
             InteractionTalk inter2 = new InteractionTalk();
