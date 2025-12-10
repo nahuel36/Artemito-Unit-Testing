56192b3d1d0e2e378f2aaafec238948e04e32f0f
nahuel36 <nahuel36gg@gmail.com>
2025-12-10 19:45:21 -0300

    Clean up CursorBecameVisible by removing empty methods
    Removed unused Start and Update methods.

--- Assets/Scripts/PointAndClick/UI/CursorBecameVisible.cs
+++ Assets/Scripts/PointAndClick/UI/CursorBecameVisible.cs
@@ -15,15 +15,5 @@ public class CursorBecameVisible : MonoBehaviour
     {
         Cursor.visible = true;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
